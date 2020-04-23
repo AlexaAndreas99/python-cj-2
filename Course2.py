f0285d39fcb8a83be8364083589d077f7aa4eea4
unknown <alexaandreas99@gmail.com>
2020-04-23 21:33:34 +0300

    add more examples

--- Course2.py
+++ Course2.py
@@ -4,3 +4,24 @@ b = 2
 
 c = a + b
 print("total is: {}".format(c))
+
+s = "Ana are mere"
+
+print("""acesta este un sir de caractere"
+      spart
+      pe mai multe randuri
+      """)
+
+print("introdu un nr: ")
+
+# x = int(input())
+x = 1
+y = 2
+total = x + y
+print("x+y is: {}".format(total))
+print("Path: \n {} \n {}".format(
+    "c:\\user\\Desktop\\file.py",
+    "c:\\user\\Desktop\\file2.py"))
+
+print("="*100)
+
